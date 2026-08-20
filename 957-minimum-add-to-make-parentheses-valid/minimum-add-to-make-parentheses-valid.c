2d973f13561415af1fa34208a190ce7c0097ad55
lingnn <99548146+lingnn@users.noreply.github.com>
2026-08-20 10:55:05 +0700

    Time: 0 ms (100.00%) | Memory: 8.6 MB (55.36%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.c
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.c
@@ -1,10 +1,10 @@
 int minAddToMakeValid(char* s) {
     int open = 0, close = 0;
     for(int i = 0;i < strlen(s);i ++){
-        if(*(s + i) == '('){
+        if(s[i] == '('){
             open++;
         }
-        else if(*(s+i) == ')' && open >0){
+        else if(s[i] == ')' && open >0){
             open--;
         }
         else
